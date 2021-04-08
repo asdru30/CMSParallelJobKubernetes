7024a40c0ad4e5d32b839c4b0863d5968f446733
Asdrubal Cruz <45265594+asdru30@users.noreply.github.com>
2021-04-08 10:04:28 -0500

    Add files via upload

---
+++ merge.C
@@ -0,0 +1,15 @@
+#include "TFile.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TString.h"
+#include <iostream>
+
+void mymerge() {
+TChain ch("aod2nanoaod/Events");
+for (int i = 1; i < 3; ++i)
+  {
+    TString filename = TString::Format("output%i.root", i);
+    ch.Add(filename);
+  }
+ch.Merge("big.root");
+}
